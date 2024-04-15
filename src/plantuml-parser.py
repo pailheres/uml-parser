fa241739d060b5db5cbc4eac3352515ccddd8d8a
cmercier <christian.mercier@r2.ca>
2024-04-15 16:59:18 -0400

    ok pour les classes

--- src/plantuml-parser.py
+++ src/plantuml-parser.py
@@ -12,98 +12,140 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 import logging
-import pprint
 import os
 import json
 import lark
-
 
 #from lark import Lark
 from sys import argv
 from jinja2 import Environment, FileSystemLoader
 
-# Define a function to recursively convert the Lark tree to a nested structure
-def tree_to_dict(item):
-    the_node = 'blableblibloblu'
-    the_data = None
+import pprint
+pp = pprint.PrettyPrinter(indent=1)
 
-    if isinstance(item, lark.Tree):
-        if isinstance(item.data, lark.Token):
-            if item.data.type == 'RULE':
-                if item.data.value != '' :
-                    the_node = item.data.value
-                    the_data = [tree_to_dict(child) for child in item.children]
-    else :
-        if isinstance(item, lark.Token):
-            the_node = item.type
-            the_data = item.value
+# Define a custom transformer to process a PlantUml Tree
+class ProcessPlantUmlTree(lark.visitors.Transformer):
+    def __default__(self, data, children, meta):
+        # Default method to handle unknown rules
+        return children or data
 
-    return {
-        the_node : the_data
-    }
+    def VISIBILITY(self, item):
+        visi = 'private' if item.value == '-' else 'protected' if item.value == '#' else 'public'
+        return {item.type : visi}
 
-def tree_to_class(item):
-    if isinstance(item, lark.Tree):
-        write(f'{indent}{{ "type": "{item.data}", "children": [\n')
-        sep = ''
-        for child in item.children:
-            write(indent)
-            write(sep)
-            _tree_to_json(child, write, level)
-            sep = ',\n'
-        write(f'{indent}] }}\n')
-    elif isinstance(item, lark.Token):
-        bla
+    def FUNC_NAME(self, item):
+        return {item.type : item.value}
 
-def tree_to_struct(item):
-    if isinstance(item, lark.Tree):
-        write(f'{indent}{{ "type": "{item.data}", "children": [\n')
-        sep = ''
-        for child in item.children:
-            write(indent)
-            write(sep)
-            _tree_to_json(child, write, level)
-            sep = ',\n'
-        write(f'{indent}] }}\n')
-    elif isinstance(item, lark.Token):
-        # reminder: Lark Tokens are directly strings
-        # token attrs include: line, end_line, column, end_column, pos_in_stream, end_pos
-        write(f'{indent}{{ "type": "{item.type}", "text": "{item}", "line": {item.line}, "col": {item.column} }}\n')
-    #else:
-        #print ("qwerty" + item)
-        #assert False, item  # fall-through
+    def PARAM_NAME(self, item):
+        return {item.type : item.value}
 
+    def VAR_NAME(self, item):
+        return {item.type : item.value}
 
-#lark_tree_to_json is from https://gist.github.com/charles-esterbrook/9ab557d70391fd85ebac2b1a59a326cf
-def tree_to_json_str(item):
-    output = []
-    tree_to_json(tree, output.append)  # will build output in memory
-    return ''.join(output)
+    def CLASS_NAME(self, item):
+        return {item.type : item.value}
 
-def tree_to_json(item, write=None):
-    """ Writes a Lark tree as a JSON dictionary. """
-    if write is None: write = sys.stdout.write
-    _tree_to_json(item, write, 0)
+    def BASE_CLASS_NAME(self, item):
+        return {item.type : item.value}
 
-def _tree_to_json(item, write, level):
-    indent = '  ' * level
-    level += 1
-    if isinstance(item, lark.Tree):
-        write(f'{indent}{{ "type": "{item.data}", "children": [\n')
-        sep = ''
-        for child in item.children:
-            write(indent)
-            write(sep)
-            _tree_to_json(child, write, level)
-            sep = ',\n'
-        write(f'{indent}] }}\n')
-    elif isinstance(item, lark.Token):
-        # reminder: Lark Tokens are directly strings
-        # token attrs include: line, end_line, column, end_column, pos_in_stream, end_pos
-        write(f'{indent}{{ "type": "{item.type}", "text": "{item}", "line": {item.line}, "col": {item.column} }}\n')
-    #else:
-        #print ("qwerty" + item)
-        #assert False, item  # fall-through
+    def TYPE(self, item):
+        return {item.type : item.value}
+
+    def variable(self, item):
+        result = {}
+        for d in item:
+            result.update(d)
+        return {'variable' : result}
+
+    def param(self, item):
+        return item  #ca va dans param_list, donc pas besoin d'en faire un {key: value}, on sait deja que la liste contient juste des params
+
+    def param_list(self, item):
+        result = []
+        for L in item:
+            the_dict = {}
+            for d in L:
+                the_dict.update(d)
+            result.append(the_dict)
+        return {'param_list' : result}
+
+    def function(self, item):
+        result = {}
+        for d in item:
+            result.update(d)
+        return {'function' : result}
+
+    def vari_or_func(self, item):
+        #vari_or_func_dict = next((d for d in item if any(key in d for key in ['variable', 'function'])), None)
+        vari_dict = next((d for d in item if any(key in d for key in ['variable'])), None)
+        func_dict = next((d for d in item if any(key in d for key in ['function'])), None)
+        visi_dict = next((d for d in item if any('VISIBILITY')), None)
+        #vari_or_func_dict.update(visi_dict)
+        if (vari_dict != None):
+            vari_dict['variable'].update(visi_dict)
+        if (func_dict != None):
+            func_dict['function'].update(visi_dict)
+        return vari_dict or func_dict
+
+    def vari_or_func_list(self, item):
+        the_keys = ['variable', 'function']
+        the_keys_dict = {}
+        for key in the_keys:
+            the_keys_dict[key] = {}
+        result = []
+        for d in item:  # loop the dictionaries list
+            for key in the_keys:
+                if key in d:
+                    if d[key]['VISIBILITY'] not in the_keys_dict[key]:
+                        the_keys_dict[key][d[key]['VISIBILITY']] = []
+                    the_keys_dict[key][d[key]['VISIBILITY']].append(d[key])
+        return the_keys_dict
+
+    def class_def(self, item):
+        result = {}
+        for d in item:
+            result.update(d)
+        return {'class' : result}
+
+    def RELATION_ARROW(self, item):
+        arrow = 'extension' if item.value == '<|--' else 'composition' if item.value == '*--' else 'aggregation' if item.value == 'o--' else 'use'
+        return {item.type : arrow}
+
+    def RELATION_CLASS_NAME(self, item):
+        return {item.type : item.value}
+        
+    def RELATION_LABEL(self, item):
+        return {item.type : item.value}
+
+    def RELATION_TEXT(self, item):
+        return {item.type : item.value}
+
+    def relationship(self, item):
+        result = []
+        print('A')
+        print(item)
+        #for L in item:
+        #    the_dict = {}
+        #    for d in L:
+        #        the_dict.update(d)
+        #    result.append(the_dict)
+        return {'relationship' : result}
+
+    def start(self, item):
+        the_model = {}
+        the_keys = ['class']
+        for key in the_keys:
+            the_model[key] = []
+
+        for d in item:
+            for key in the_keys:
+                if key in d:
+                    the_model[key].append(d)
+
+        #print("z")
+        #print(the_model)
+        return the_model
+    
 
 def getopts(argv):
     opts = {}  # Empty dictionary to store key-value pairs.
@@ -120,78 +162,6 @@ def getopts(argv):
         # Reduce the argument list by copying it starting from index 1.
         argv = argv[1:]
     return opts
-
-# Define a custom transformer to process a PlantUml Tree
-class ProcessPlantUmlTree2(lark.visitors.Visitor):
-    start = dict
-
-class s_class:
-    variables = {}
-
-# Define a custom transformer to process a PlantUml Tree
-class ProcessPlantUmlTree(lark.visitors.Transformer):
-    #def __default__(self, data, children, meta):
-    #    # Default method to handle unknown rules
-    #    return children or data
-    #
-    #def start(self, tree):
-    #    #print(tree)
-    #    return tree
-    #    
-    #def class_def(self, item):
-    #    for bla in item :
-    #        print(bla)
-    #        if isinstance(bla, lark.Tree):
-    #            print("blablabla:"+bla.data)
-    #        elif isinstance(bla, lark.Token):
-    #            print("blebleble:"+bla.type)
-    #       
-    #    print(item)
-    #
-    #    return item
-        
-
-    def variable(self, item):
-        print("a")
-#        for terminal in item.children:
-#            if isinstance(terminal, lark.Token):
-#                if terminal.type == 'VAR_NAME'
-
-        if len(item) == 2:
-            if isinstance(item[0], lark.Token):
-                if isinstance(item[1], lark.Token):
-                    print(item)
-                    return item[0], item[1]
-
-    def var(self, item):
-        print("b")
-        if len(item) == 1:
-            print(item)
-            return item[0].value
-
-    def type(self, item):
-        print("c")
-        if len(item) == 1:
-            print(item)
-            return item[0].value
-
-    #def variable(self, children):
-    #    return "the variable"
-    #
-    #def type(self, tree):
-    #    print(tree.data)
-    #    print(tree.children)
-    #    #for child in tree.children:
-    #    #    self.visit(child)
-    #
-    #def method(self, children):
-    #    return "the method"
-    #
-    #def relationship(self, children):
-    #    return "the relationship"
-    #
-    #def skinparam(self, children):
-    #    return "the skinparam"
 
 if __name__ == '__main__':
     myargs = getopts(argv)
@@ -214,83 +184,33 @@ if __name__ == '__main__':
         logging.basicConfig(level=logging.INFO)
 
     tree = parser.parse(f.read())
-    print(tree.pretty())
-    print ("-----\n")
-    print ("-----\n")
-    print(tree)
-    print ("-----\n")
-    print ("-----\n")
-    print ("-----\n")
-    print ("-----\n")
-    print ("-----\n")
+    #print(tree.pretty())
+    #print ("-----\n")
+    #print ("-----\n")
+    #print(tree)
+    pp.pprint(tree)
+    #print ("-----\n")
+    #print ("-----\n")
+    #print ("-----\n")
+    #print ("-----\n")
+    #print ("-----\n")
+
     #ProcessPlantUmlTree2().visit_topdown(tree)
-    #ProcessPlantUmlTree().transform(tree)
-
-    # Convert Lark tree to nested dictionary
-    tree_dict = tree_to_dict(tree)
-    print(tree_dict)
-
-#    print(tree.pretty())
+    the_model = ProcessPlantUmlTree().transform(tree)
+    #print(tree.pretty())
+    pp.pprint(the_model)
+    ## Convert Lark tree to nested dictionary
+    #tree_dict = tree_to_dict(tree)
+    #print(tree_dict)
     
 #    json_str = tree_to_json_str(tree)
 #    print(json_str)
-#    
+
     environment = Environment(loader=FileSystemLoader("src/templates/"))
     template_class_hpp = environment.get_template("template_class_hpp.txt")
-#    
-#    
-#    the_classes = [{
-#        'class_name' : 'qwerty',
-#        'functions' : [{
-#            'name' : 'f_func',
-#            'return_type' : 'void',
-#            'params' : [{
-#                'type' : 'UINT8',
-#                'name' : 'a_param'
-#            }
-#        }]
-#    }]
-#    
-    the_start = {'classes':[]}
-    if 'start' in tree_dict:
-        for item in tree_dict['start']:
-            if 'class_def' in item:
-                the_class = None
-                for class_item in item['class_def']:
-                    print(class_item)
-                    if 'CLASS_NAME' in class_item:
-                        print(class_item['CLASS_NAME'])
-                        the_class = {
-                            'CLASS_NAME' : class_item['CLASS_NAME'],
-                            'var' : {'public':[], 'protected':[], 'private':[]},
-                            'func' : {'public':[], 'protected':[], 'private':[]}
-                        }
-                    if 'vari_or_func' in class_item:
-                        for varfunc_item in class_item['vari_or_func']:
-                            if 'VISIBILITY' in varfunc_item:
-                                visibility = 'public'
-                                if varfunc_item['VISIBILITY'] == '#':
-                                    visibility = 'protected'
-                                elif varfunc_item['VISIBILITY'] == '-':
-                                    visibility = 'private'
-                                    
-                                print(visibility)
-                                if 'variable' in varfunc_item:
-                                    for var_item in varfunc_item['variable']:
-                                        if 'VAR_NAME' in var_item:
-                                            if 'TYPE' in var_item:
-                                                the_class['var'][visibility].append({'VAR_NAME': var_item['VAR_NAME'], 'TYPE' : var_item['TYPE']})
-                                                    
-                                                    
-                                                
-                                    
-                                
 
-                the_start['classes'].append(the_class)
-            
-    print(the_start)
-        
-    for the_class in the_start['classes']:
+    for the_class_dict in the_model['class']:
+        the_class = the_class_dict['class']
         filename = f"out/{the_class['CLASS_NAME'].lower()}.hpp"
         content = template_class_hpp.render(the_class)
         with open(filename, mode='w', encoding='utf-8') as message:
